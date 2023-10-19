f996694e5258abb87496ffcba04d7fe4fc566c5a
hsaruJ <jurash232@gmail.com>
2023-10-19 13:46:20 +0300

    feat: billion seconds from birth

--- Any_simple_task.py
+++ Any_simple_task.py
@@ -7,7 +7,7 @@ except ValueError:
     print("Ошибка: не соблюдён формат ввода. Перезапустите, чтобы продолжить.")
     exit()
 
-
+day_of_birth = ()
 try:
     day_of_birth = datetime.datetime(year, month, day, hour=12)
 except ValueError:
